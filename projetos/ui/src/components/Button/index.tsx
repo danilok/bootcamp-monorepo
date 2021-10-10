e0a97e3bda6cf238d9003e39568abb9442b218e7
Danilo Yorinori <daniloksy@gmail.com>
2021-10-10 19:27:07 -0300

    feat(button): add style with ButtonWrapper

--- projetos/ui/src/components/Button/index.tsx
+++ projetos/ui/src/components/Button/index.tsx
@@ -1,4 +1,22 @@
 import React from 'react';
+import styled from 'styled-components';
+import get from 'lodash/get';
+
+const ButtonWrapper = styled.button`
+    border: 0;
+    cursor: pointer;
+    padding: 12px 26px;
+    font-weight: bold;
+    opacity: 1;
+    border-radius: 8px;
+    color: ${({ theme, color }) => get(theme, `colors.${color}.contrastText`)};
+    background: ${({ theme, color }) => get(theme, `colors.${color}.color`)};
+    &:disabled {
+      cursor: not-allowed;
+      opacity: 0.2;
+    }
+`;
+
 
 interface ButtonProps {
   /**
@@ -9,16 +27,21 @@ interface ButtonProps {
    * Habilita/desabilita o botão
    */
   disabled?: boolean;
+  /**
+   * Cor do botão
+   */
+  color: string;
 }
 
-export default function Button({ children, disabled }: ButtonProps) {
-  return(
-    <button disabled={disabled}>
+export default function Button({ children, disabled, color }: ButtonProps) {
+  return (
+    <ButtonWrapper disabled={disabled} color={color}>
       {children}
-    </button>
+    </ButtonWrapper>
   );
 }
 
 Button.defaultProps = {
   disabled: false,
+  color: 'primary.main',
 }
